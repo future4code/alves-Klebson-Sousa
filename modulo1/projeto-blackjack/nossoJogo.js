d1c9ec63afbb2ebeb243f1b88998f00823529954
Klebson Bicalho de Sousa <klebson.sis@gmail.com>
2022-05-20 22:38:21 -0300

    Corrigindo commit

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,27 +10,27 @@
  * 
  * 
  */
-console.log("Boas vindas ao jogo de Blackjack!")
-const carta = comprarCarta();
-if (confirm("Quer iniciar uma nova rodada?")) {
-   // confirm("Compre uma carta")
-   const carta2 = comprarCarta();
-   const pontuacaoUsuario = carta2.valor + carta.valor
-   const carta3 = comprarCarta()
-   const carta4 = comprarCarta()
-   const pontuacaoComputador = carta3.valor + carta4.valor
-
-   console.log(`Usuário - cartas: ${carta.texto} ${carta2.texto} - pontuação ${pontuacaoUsuario}`) 
-   console.log(`Computador - cartas: ${carta3.texto} ${carta4.texto} - pontuação ${pontuacaoComputador}`) 
-   if (pontuacaoComputador > pontuacaoUsuario) {
-      console.log("O computador ganhou!")
-   } else if (pontuacaoComputador < pontuacaoUsuario) {
-      console.log("O usuário ganhou!")
-   } else {
-      console.log("Empate!")
-   }
-// console.log(carta.valor, carta2.valor) 
-// imprime o valor da carta (um número). Nesse caso: 10)
-} else {
-   console.log("O jogo acabou")
-}
+    console.log("Boas vindas ao jogo de Blackjack!")
+    const carta = comprarCarta();
+    if (confirm("Quer iniciar uma nova rodada?")) {
+       // confirm("Compre uma carta")
+       const carta2 = comprarCarta();
+       const pontuacaoUsuario = carta2.valor + carta.valor
+       const carta3 = comprarCarta()
+       const carta4 = comprarCarta()
+       const pontuacaoComputador = carta3.valor + carta4.valor
+    
+       console.log(`Usuário - cartas: ${carta.texto} ${carta2.texto} - pontuação ${pontuacaoUsuario}`) 
+       console.log(`Computador - cartas: ${carta3.texto} ${carta4.texto} - pontuação ${pontuacaoComputador}`) 
+       if (pontuacaoComputador > pontuacaoUsuario) {
+          console.log("O computador ganhou!")
+       } else if (pontuacaoComputador < pontuacaoUsuario) {
+          console.log("O usuário ganhou!")
+       } else {
+          console.log("Empate!")
+       }
+    // console.log(carta.valor, carta2.valor) 
+    // imprime o valor da carta (um número). Nesse caso: 10)
+    } else {
+       console.log("O jogo acabou")
+    }

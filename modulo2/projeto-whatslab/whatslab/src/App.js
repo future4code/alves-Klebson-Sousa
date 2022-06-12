7c712611b516fc39fce46dcb565cc3aa845bf8a8
Klebson Bicalho de Sousa <klebson.sis@gmail.com>
2022-06-12 20:57:20 -0300

    Fazendo algumas modificações

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,62 +1,12 @@
 import styled from "styled-components";
 import React from "react";
-import Avatar from "./Components/img/avatar.png"
+import { MainContainer, Header, ImgAvatar, MessageArea, Chat, Chat2, Footer, 
+Input1, Input2, Botao } from "./Components/Styled.js"
+import Avatar from "./Components/img/avatar.png";
+import "./Components/EnterBotao.js"
 
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  box-sizing: border-box;
-  flex-direction: column;
-  align-items: center;
-  background-color: #ffffff;
-  margin-top: 0;
-`;
-const Heder = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 60%;
-  padding: 20px 0;
-  background-color: #f0f2f5;
-`
-const ImgAvatar = styled.img`
-  width: 40px;
-  height: 40px;
-  border-radius: 20px;
-  cursor: pointer;
-`
-const Chat = styled.div`
-  width: 60%;
-  min-height: 75vh;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-end;
-  overflow-y: hidden;
-  background-color: #eae6df;
-`;
-const Footer = styled.footer`
-  display: flex;
-  flex-direction: row;
-  width: 60%;
-  padding: 25px 0;
-  background-color: #f0f2f5;
-`;
-const Input1 = styled.input`
-  width: 25%;
-  padding: auto;
-  background-color: #ffffff;
-  border-radius: 5%;
-`;
-const Input2 = styled.input`
-  width: 60%;
-  padding: auto;
-  background-color: #ffffff;
-`;
-const Botao = styled.button`
-  background-color: #ffffff;
-  width: 15%;
-`;
+
+
 class App extends React.Component {
   state = {
     usuarios: [],
@@ -87,24 +37,28 @@ class App extends React.Component {
     const listaComponentes = this.state.usuarios.map((usuario) => {
       const nome = usuario.nome.toLowerCase();
       if (nome === "eu") {
-        return <p>{usuario.mensagem}</p>;
+        return (
+          <Chat2>
+            <p>{usuario.mensagem}</p>
+          </Chat2>
+        );
       } else {
         return (
-          <p>
-            <strong>{usuario.nome}:</strong> {usuario.mensagem}
-          </p>
+          <Chat>
+            <p>
+              <strong>{usuario.nome}:</strong> {usuario.mensagem}
+            </p>
+          </Chat>
         );
       }
     });
 
     return (
       <MainContainer>
-        <Heder>
-          <ImgAvatar src={Avatar} alt="Imagem do usuário"/>
-        </Heder>
-        <Chat>
-          {listaComponentes}
-          </Chat>
+        <Header>
+          <ImgAvatar src={Avatar} alt="Imagem do usuário" />
+        </Header>
+        <MessageArea>{listaComponentes}</MessageArea>
         <Footer>
           <Input1
             value={this.state.valorInputNome}
@@ -116,7 +70,7 @@ class App extends React.Component {
             onChange={this.onChangeInputMensagem}
             placeholder={"Mensagem"}
           />
-          <Botao onClick={this.adicionarUsuario}>Enviar</Botao>
+          <Botao onClick={this.adicionarUsuario} id="send">Enviar</Botao>
         </Footer>
       </MainContainer>
     );

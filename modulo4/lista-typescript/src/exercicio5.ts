98e7aea64bdddda54f4cff482f6a38591fc64698
Klebson Bicalho de Sousa <klebson.sis@gmail.com>
2022-08-14 22:46:57 -0300

    Continuação dos exercícios

--- modulo4/lista-typescript/src/exercicio5.ts
+++ modulo4/lista-typescript/src/exercicio5.ts
@@ -1,11 +1,32 @@
 // 5. Considerando o array de usuários abaixo, crie uma função que receba este array como 
 // parâmetro e retorne uma lista com apenas os emails dos usuários “admin”. 
 
-[
-	{name: "Rogério", email: "roger@email.com", role: "user"},
-	{name: "Ademir", email: "ademir@email.com", role: "admin"},
-	{name: "Aline", email: "aline@email.com", role: "user"},
-	{name: "Jéssica", email: "jessica@email.com", role: "user"},  
-	{name: "Adilson", email: "adilson@email.com", role: "user"},  
-	{name: "Carina", email: "carina@email.com", role: "admin"}      
-] 
+enum ROLE {
+	USER = "user",
+	ADMIN = "admin"
+}
+
+type Users = {
+	name: string,
+	email: string,
+	role: ROLE
+}
+
+const pessoas: Users[] = [
+	{name: "Rogério", email: "roger@email.com", role:ROLE.USER},
+	{name: "Ademir", email: "ademir@email.com", role: ROLE.ADMIN},
+	{name: "Aline", email: "aline@email.com", role: ROLE.USER},
+	{name: "Jéssica", email: "jessica@email.com", role: ROLE.USER},  
+	{name: "Adilson", email: "adilson@email.com", role: ROLE.USER},  
+	{name: "Carina", email: "carina@email.com", role: ROLE.ADMIN}      
+]
+
+const listaEmails = ((array: Users[]) => {
+	const emailAdmin = array.filter((pessoa: Users) => {
+		return pessoa.role === "admin"
+	}).map((pessoa:Users) => {
+		return pessoa.email
+	})
+	return emailAdmin
+})
+console.log(listaEmails(pessoas))
